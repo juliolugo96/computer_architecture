089a905d2726c81c273ec8d26abe63f4a0e2e974
Luis Adolfo Ramírez Inciarte <donlarry.timoteo@gmail.com>
2019-10-27 19:58:03 -0400

    And gate added and Makefile upgraded ;)

---
+++ Logic Gates/and.h
@@ -0,0 +1,20 @@
+#ifndef AND_GATE
+#define AND_GATE
+
+SC_MODULE (And) {
+
+  sc_in <bool> a;
+  sc_in <bool> b;
+  sc_out <bool> c;
+
+  void fun () {
+    c.write ( a.read() & b.read() );
+  }
+
+  SC_CTOR (And) {
+    SC_METHOD (fun);
+    sensitive << a << b;
+  }
+};
+
+#endif
